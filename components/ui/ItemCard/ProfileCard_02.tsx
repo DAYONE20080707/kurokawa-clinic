ef30222deec1a8a1b4632563e7f767f0980f5cb5
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-09-16 08:13:17 +0900

    top profile

--- components/ui/ItemCard/ProfileCard_02.tsx
+++ components/ui/ItemCard/ProfileCard_02.tsx
@@ -11,13 +11,9 @@ const ProfileCard_02 = ({ label, value }: ProfileCard_02Props) => {
   return (
     <dl className=" border-b border-[#d9d9d9] flex gap-4 pb-4">
       {label && (
-        <dt className="text-base font-medium ![line-height:160%]">
-          {label}
-        </dt>
+        <dt className="text-base font-medium ![line-height:160%]">{label}</dt>
       )}
-      <dd className="text-base font-medium ![line-height:160%]">
-        {value}
-      </dd>
+      <dd className="text-base font-medium ![line-height:160%]">{value}</dd>
     </dl>
   )
 }
